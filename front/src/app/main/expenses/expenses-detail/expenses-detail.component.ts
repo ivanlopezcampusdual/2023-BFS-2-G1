743a394e63d4390089f692a574a790bab8f0a058
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-10-03 14:12:51 +0200

    SP8: Validación números negativos (CA3)

--- front/src/app/main/expenses/expenses-detail/expenses-detail.component.ts
+++ front/src/app/main/expenses/expenses-detail/expenses-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidatorFn, ValidationErrors, FormControl } from "@angular/forms";
+
+import { OValidators } from "ontimize-web-ngx";
 
 @Component({
-  selector: 'app-expenses-detail',
-  templateUrl: './expenses-detail.component.html',
-  styleUrls: ['./expenses-detail.component.css']
+  selector: "app-expenses-detail",
+
+  templateUrl: "./expenses-detail.component.html",
+
+  styleUrls: ["./expenses-detail.component.css"],
 })
 export class ExpensesDetailComponent implements OnInit {
+  valor: number = 0;
 
-  constructor() { }
+  validatorAmount: ValidatorFn[] = [];
 
-  ngOnInit() {
+  constructor() {
+    this.validatorAmount.push(
+      OValidators.patternValidator(/^\d+(\.\d{1,2})?$/, "negativeNumber")
+    );
   }
 
+  ngOnInit() {}
 }
