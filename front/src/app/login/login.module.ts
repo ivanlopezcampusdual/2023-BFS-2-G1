9355c41e20a9860c6a1ffacbba8dadad433a7bcc
Ross-X <Rossi Andreina Perez Araque>
2023-11-02 10:43:04 +0100

    SP55: Modificacion del login, creacion de animacion y de componente terms.

--- front/src/app/login/login.module.ts
+++ front/src/app/login/login.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { NgModule } from "@angular/core";
+import { OntimizeWebModule } from "ontimize-web-ngx";
 
-import { SharedModule } from '../shared/shared.module';
-import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './login.component';
+import { SharedModule } from "../shared/shared.module";
+import { LoginRoutingModule } from "./login-routing.module";
+import { LoginComponent } from "./login.component";
+import { TermsComponent } from "./terms/terms.component";
 
 @NgModule({
-  imports: [
-    SharedModule,
-    OntimizeWebModule,
-    LoginRoutingModule
-  ],
-  declarations: [
-    LoginComponent
-  ]
+  imports: [SharedModule, OntimizeWebModule, LoginRoutingModule],
+  declarations: [LoginComponent, TermsComponent],
 })
-export class LoginModule { }
+export class LoginModule {}
