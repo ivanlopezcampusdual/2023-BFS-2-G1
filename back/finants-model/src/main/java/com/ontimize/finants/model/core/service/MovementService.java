09716b1a1521a49e6c8408472069c382fa294559
Alba Sousa <malbasogo@gmail.com>
2023-11-03 11:58:00 +0100

    FIN47: Creo copia de los mapas traidos desde front y modifico métodos para cambio de signo y y cambio de idGrupo de NullValue a null

--- back/finants-model/src/main/java/com/ontimize/finants/model/core/service/MovementService.java
+++ back/finants-model/src/main/java/com/ontimize/finants/model/core/service/MovementService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,20 +29,23 @@ public class MovementService implements IMovementService {
 
     @Override
     public EntityResult movementQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
-        return this.daoHelper.query(this.movementDao, keyMap, attrList);
+        Map<String, Object> keyMapFilterUser = new HashMap<>(keyMap);
+        keyMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.query(this.movementDao, keyMapFilterUser, attrList);
     }
 
     @Override
     public EntityResult movementInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
-        attrMap.put(MovementDao.ATTR_USER_, this.daoHelper.getUser().getUsername());
-        return this.daoHelper.insert(this.movementDao, attrMap);
+        Map<String, Object> attrMapFilterUser = new HashMap<>(attrMap);
+        attrMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.insert(this.movementDao, attrMapFilterUser);
     }
 
     @Override
     public EntityResult movementUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_USER_, this.daoHelper.getUser().getUsername());
-        return this.daoHelper.update(this.movementDao, attrMap, keyMap);
+        Map<String, Object>attrMapFilterUser = new HashMap<>(attrMap);
+        attrMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.update(this.movementDao, attrMapFilterUser, keyMap);
     }
 
     @Override
@@ -57,49 +61,52 @@ public class MovementService implements IMovementService {
 
     @Override
     public EntityResult totalIncomesForCurrentMonth(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_MOV_MONTH, LocalDate.now().getMonthValue());
-        keyMap.put(MovementDao.ATTR_MOV_YEAR, LocalDate.now().getYear());
-        return this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_SUM_INCOMES_AMOUNT_FOR_MONTH);
+        Map<String, Object> filterDatekeyMap = new HashMap<>(keyMap);
+        filterDatekeyMap.put(MovementDao.ATTR_MOV_MONTH, LocalDate.now().getMonthValue());
+        filterDatekeyMap.put(MovementDao.ATTR_MOV_YEAR, LocalDate.now().getYear());
+        return this.daoHelper.query(this.movementDao, filterDatekeyMap, attrList, MovementDao.QUERY_SUM_INCOMES_AMOUNT_FOR_MONTH);
     }
     @Override
     public EntityResult totalExpensesForCurrentMonth(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_MOV_MONTH, LocalDate.now().getMonthValue());
-        keyMap.put(MovementDao.ATTR_MOV_YEAR, LocalDate.now().getYear());
-        return this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_SUM_EXPENSES_AMOUNT_FOR_MONTH);
+        Map<String, Object> filterDatekeyMap = new HashMap<>(keyMap);
+        filterDatekeyMap.put(MovementDao.ATTR_MOV_MONTH, LocalDate.now().getMonthValue());
+        filterDatekeyMap.put(MovementDao.ATTR_MOV_YEAR, LocalDate.now().getYear());
+        return this.daoHelper.query(this.movementDao, filterDatekeyMap, attrList, MovementDao.QUERY_SUM_EXPENSES_AMOUNT_FOR_MONTH);
     }
 
     public EntityResult insertExpenseInsert (Map<String,Object> attrMap) throws OntimizeJEERuntimeException{
-        Float movAmount = (Float)attrMap.get(MovementDao.ATTR_MOV_AMOUNT);
-        Double expenseAmount = movAmount.doubleValue() * -1;
-        attrMap.put(MovementDao.ATTR_MOV_AMOUNT, expenseAmount);
-        return movementInsert(attrMap);
+        Map<String, Object> attrMapForThisQuery =  new HashMap<>(attrMap);
+        Float movAmount = (Float) attrMapForThisQuery.get(MovementDao.ATTR_MOV_AMOUNT);
+        attrMapForThisQuery.put(MovementDao.ATTR_MOV_AMOUNT, changeSignMovAmount(movAmount));
+        return movementInsert(attrMapForThisQuery);
     }
 
     @Override
     public EntityResult totalExpensesAmountDayQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
-        return this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_TOTAL_EXPENSES_AMOUNT_DAY);
+        Map<String,Object> keyMapFilterUser = new HashMap<>(keyMap);
+        keyMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.query(this.movementDao, keyMapFilterUser, attrList, MovementDao.QUERY_TOTAL_EXPENSES_AMOUNT_DAY);
     }
 
     @Override
     public EntityResult totalIncomesAmountDayQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
-        EntityResult totalIncomesAmountDay = this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_TOTAL_INCOMES_AMOUNT_DAY);
-        return this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_TOTAL_INCOMES_AMOUNT_DAY);
+        Map<String,Object> keyMapFilterUser = new HashMap<>(keyMap);
+        keyMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.query(this.movementDao, keyMapFilterUser, attrList, MovementDao.QUERY_TOTAL_INCOMES_AMOUNT_DAY);
     }
 
     @Override
     public EntityResult expensesForCategoriesQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
-        return this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_EXPENSES_FOR_CATEGORIES);
+        Map<String,Object> keyMapFilterUser = new HashMap<>(keyMap);
+        keyMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.query(this.movementDao, keyMapFilterUser, attrList, MovementDao.QUERY_EXPENSES_FOR_CATEGORIES);
     }
-
-
 
     @Override
     public EntityResult incomesForCategoriesQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
-        keyMap.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
-        return this.daoHelper.query(this.movementDao, keyMap, attrList, MovementDao.QUERY_INCOMES_FOR_CATEGORIES);
+        Map<String,Object> keyMapFilterUser = new HashMap<>(keyMap);
+        keyMapFilterUser.put(MovementDao.ATTR_USER_, daoHelper.getUser().getUsername());
+        return this.daoHelper.query(this.movementDao, keyMapFilterUser, attrList, MovementDao.QUERY_INCOMES_FOR_CATEGORIES);
     }
 
     @Override
@@ -126,31 +133,34 @@ public class MovementService implements IMovementService {
         if(listMovAmount == null || listMovAmount.isEmpty()){
             return BigDecimal.ZERO;
         }else{
-            BigDecimal balance = listMovAmount.stream()
+            return listMovAmount.stream()
                     .reduce(BigDecimal.ZERO,BigDecimal::add );
-            return balance;
         }
     }
 
     @Override
     public EntityResult expensesForCategoriesUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
-        changeSignMovAmount(attrMap);
-        checkIdGroup(attrMap);
-        return this.movementUpdate(attrMap,keyMap);
+        Map<String, Object> attrMapForThisQuery =  new HashMap<>(attrMap);
+        Float movAmount = (Float) attrMapForThisQuery.get(MovementDao.ATTR_MOV_AMOUNT);
+        if(movAmount != null) {
+            Double movAmountSignNegative = changeSignMovAmount(movAmount);
+            attrMapForThisQuery.put(MovementDao.ATTR_MOV_AMOUNT, movAmountSignNegative);
+        }
+        Object groupId = changeFromNullValueToNull(attrMapForThisQuery);
+        attrMapForThisQuery.put(MovementDao.ATTR_GR_ID, groupId);
+        return this.movementUpdate(attrMapForThisQuery,keyMap);
     }
 
-    private static void checkIdGroup(Map<String, Object> attrMap) {
-        if(attrMap.get(MovementDao.ATTR_GR_ID) instanceof NullValue){
-            attrMap.replace(MovementDao.ATTR_GR_ID, null);
-        }
+    private static boolean isIdGroupNull(Map<String, Object> attrMapForThisQuery) {
+        return attrMapForThisQuery.get(MovementDao.ATTR_GR_ID) instanceof NullValue;
+    }
+    private static Object changeFromNullValueToNull(Map<String, Object> attrMapForThisQuery){
+      return (MovementService.isIdGroupNull(attrMapForThisQuery))? null : attrMapForThisQuery.get(MovementDao.ATTR_GR_ID);
     }
 
-    private static void changeSignMovAmount(Map<String, Object> attrMap) {
-        Float movAmount = (Float) attrMap.get(MovementDao.ATTR_MOV_AMOUNT);
-        if(movAmount != null){
-            Double expenseAmount = movAmount.doubleValue() * -1;
-            attrMap.put(MovementDao.ATTR_MOV_AMOUNT, expenseAmount);
-        }
+    private static Double changeSignMovAmount(Float movAmount) {
+          double movAmountCastAndSignNegative = movAmount *= -1;
+          return movAmountCastAndSignNegative;
     }
 
     @Override
@@ -160,8 +170,10 @@ public class MovementService implements IMovementService {
 
     @Override
     public EntityResult incomesForCategoriesUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
-        checkIdGroup(attrMap);
-        return this.movementUpdate(attrMap,keyMap);
+        Map<String, Object> attrMapForThisQuery =  new HashMap<>(attrMap);
+        Object groupId = changeFromNullValueToNull(attrMapForThisQuery);
+        attrMapForThisQuery.put(MovementDao.ATTR_GR_ID, groupId);
+        return this.movementUpdate(attrMapForThisQuery,keyMap);
     }
 
     @Override
