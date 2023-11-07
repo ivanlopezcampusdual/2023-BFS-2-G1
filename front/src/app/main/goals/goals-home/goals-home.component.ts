b4ba33e35406aacc0da402b34e54373987024f08
Ross-X <Rossi Andreina Perez Araque>
2023-11-07 20:09:16 +0100

    Se eliminan botones que no tienen funcionalidad

--- front/src/app/main/goals/goals-home/goals-home.component.ts
+++ front/src/app/main/goals/goals-home/goals-home.component.ts
@@ -1,26 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartSeries, PieChartConfiguration } from 'ontimize-web-ngx-charts';
-
+import { Component, OnInit } from "@angular/core";
+import { ChartSeries, PieChartConfiguration } from "ontimize-web-ngx-charts";
 
 @Component({
-  selector: 'app-goals-home',
-  templateUrl: './goals-home.component.html',
-  styleUrls: ['./goals-home.component.css']
+  selector: "app-goals-home",
+  templateUrl: "./goals-home.component.html",
+  styleUrls: ["./goals-home.component.css"],
 })
 export class GoalsHomeComponent implements OnInit {
-
   chartParameters: PieChartConfiguration;
 
   constructor() {
-  
     this.chartParameters = new PieChartConfiguration();
-
-    this.chartParameters.labelType="percent";
-    this.chartParameters.legendPosition="bottom";   
-    
+    this.chartParameters.labelType = "percent";
+    this.chartParameters.legendPosition = "bottom";
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
