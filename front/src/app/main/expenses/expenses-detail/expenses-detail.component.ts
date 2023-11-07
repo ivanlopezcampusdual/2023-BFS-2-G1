2ccd3dd6794df6759775e566b8f636d2a4e3e23b
Ross-X <Rossi Andreina Perez Araque>
2023-11-07 11:33:33 +0100

    SP13: El dialog edit se cierra al guardar

--- front/src/app/main/expenses/expenses-detail/expenses-detail.component.ts
+++ front/src/app/main/expenses/expenses-detail/expenses-detail.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ValidatorFn } from '@angular/forms';
-import { OValidators } from 'ontimize-web-ngx';
+import { Component, OnInit, Inject } from "@angular/core";
+import { ValidatorFn } from "@angular/forms";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { OValidators } from "ontimize-web-ngx";
 
 @Component({
-  selector: 'app-expenses-detail',
-  templateUrl: './expenses-detail.component.html',
-  styleUrls: ['./expenses-detail.component.css']
+  selector: "app-expenses-detail",
+  templateUrl: "./expenses-detail.component.html",
+  styleUrls: ["./expenses-detail.component.css"],
 })
 export class ExpensesDetailComponent implements OnInit {
   validatorAmount: ValidatorFn[] = [];
 
-  constructor() {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<ExpensesDetailComponent>
+  ) {
     this.validatorAmount.push(
-      OValidators.patternValidator(/^\d+([,.]\d+)?$/, 'negativeNumber'),
+      OValidators.patternValidator(/^\d+([,.]\d+)?$/, "negativeNumber")
     );
-    
-   }
-
-  ngOnInit() {
+  }
+  onUpdate(event: any) {
+    this.dialogRef.close();
   }
 
+  ngOnInit(): void {}
 }
