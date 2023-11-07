bbed2f021fb5453d87b076eab19789d4e3773d38
Alba Sousa <malbasogo@gmail.com>
2023-11-07 08:41:20 +0100

    Se elimina código comentado

--- front/src/app/main/home/home.component.ts
+++ front/src/app/main/home/home.component.ts
@@ -18,9 +18,7 @@ export class HomeComponent implements OnInit {
   public TOTAL_BALANCE: string = "TOTAL_BALANCE";
   public TOTAL_EXPENSE: string = "TOTAL_EXPENSE";
   public TOTAL_INCOME: string = "TOTAL_INCOME";
-  servicePath = "/balances";
-  //public MONTHLY_BALANCE: string = "MONTHLY_BALANCE";
-  //servicePath = "/balances";
+
   httpOptions: any;
   constructor(
     private router: Router,
@@ -74,11 +72,7 @@ export class HomeComponent implements OnInit {
       this.expenseBalance = data[0].expenseBalance;
     }
   }
-  // getExpenseBalance(data: { expenseBalance: number }[]) {
 
-  //   this.expenseBalance = data[0].expenseBalance || 0;
-
-  // }
   getIncomeBalance(data: { incomeBalance: number }[]) {
     if (data[0] === undefined) {
       this.incomeBalance = 0;
